9b2f0f9d10b4b743b52863a07ca3494ef173e1c9
Yvonne Zhou <yvonne.zhou@krux.com>
2014-08-28 17:53:59 -0700

    SCRUM-2384: add stats for close conn, close ps, and close rs.

--- src/main/java/com/krux/stdlib/utils/DbUtils.java
+++ src/main/java/com/krux/stdlib/utils/DbUtils.java
@@ -27,19 +27,19 @@ public class DbUtils {
     public static Connection getDbConnection() {
         ConfigProperties kprops = new ConfigProperties();
         Connection conn = null;
-        String errorMsg = null;
         long start = System.currentTimeMillis();
         try {
             Class.forName(kprops.getJdbcDriver());
             conn = DriverManager.getConnection(kprops.getJdbcUrl(), kprops.getJdbcUser(),
                     kprops.getJdbcPassword());
         } catch (Exception ex) {
-            errorMsg = ex.getMessage();
+            String errorMsg = ex.getMessage();
+            log.error("db_util", errorMsg);
             conn = null;
         }
         long time = System.currentTimeMillis() - start;
-        log.info("DB connection took " + time + "ms for whole request");
-        KruxStdLib.STATSD.time(KruxStdLib.APP_NAME + "_HTTP_200", time);
+        log.info("DB get connection took " + time + "ms.");
+        KruxStdLib.STATSD.time(KruxStdLib.APP_NAME + "_get_db_conn", time);
         return conn;
     }
 
@@ -49,14 +49,20 @@ public class DbUtils {
      * @param conn the database connection to close.
      */
     public static void close(Connection conn) {
+        long start = System.currentTimeMillis();
         if (conn != null) {
             try {
                 conn.close();
             } catch (SQLException sqe) {
                 // an error occured while closing the connection.
-                // there's really not much we can do at this point, so we eat the exception and return
+                // there's really not much we can do at this point, so we log the exception and return
+                String errorMsg = sqe.getMessage();
+                log.error("db_util", errorMsg);
             }
         }
+        long time = System.currentTimeMillis() - start;
+        log.info("DB connection close took " + time + "ms.");
+        KruxStdLib.STATSD.time(KruxStdLib.APP_NAME + "_close_db_conn", time);
     }
 
     /**
@@ -65,14 +71,20 @@ public class DbUtils {
      * @param ps the statement to close
      */
     public static void close(Statement ps) {
+        long start = System.currentTimeMillis();
         if (ps != null) {
             try {
                 ps.close();
             } catch (SQLException sqe) {
                 // an error occured while closing the statement
-                // there's really not much we can do at this point, so we eat the exception and return
+                // there's really not much we can do at this point, so we log the exception and return
+                String errorMsg = sqe.getMessage();
+                log.error("db_util", errorMsg);
             }
         }
+        long time = System.currentTimeMillis() - start;
+        log.info("DB PreparedStatement close took " + time + "ms.");
+        KruxStdLib.STATSD.time(KruxStdLib.APP_NAME + "_close_db_ps", time);
     }
 
     /**
@@ -81,13 +93,19 @@ public class DbUtils {
      * @param rs the result set to close
      */
     public static void close(ResultSet rs) {
+        long start = System.currentTimeMillis();
         if (rs != null) {
             try {
                 rs.close();
             } catch (SQLException sqe) {
                 // an error occured while closing the statement
-                // there's really not much we can do at this point, so we eat the exception and return
+                // there's really not much we can do at this point, so we log the exception and return
+                String errorMsg = sqe.getMessage();
+                log.error("db_util", errorMsg);
             }
         }
+        long time = System.currentTimeMillis() - start;
+        log.info("DB ResultSet close took " + time + "ms.");
+        KruxStdLib.STATSD.time(KruxStdLib.APP_NAME + "_close_db_rs", time);
     }
 }
