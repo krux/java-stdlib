e3eba5c0abfe58d3d9feb9adb03a4c21982bd58a
casspc <casspc@gmail.com>
2014-10-20 09:28:30 -0700

    seconds/milliseconds

---
+++ src/test/java/SimpleStuff.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class SimpleStuff {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        String time = "1413418200";
+        long t = Long.parseLong( time );
+        t = t * 1000;
+        Date d = new Date( t );
+                
+        System.out.println( d.toGMTString() );
+    }
+
+}
